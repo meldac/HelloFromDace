f4bfc5367175bdbcff0290a492ebd99c507af2fd
MandyChadwick <cha33dwick@gmail.com>
2022-02-02 19:13:22 -0900

    Hello From Mandy

--- src/Rogers/Main.java
+++ src/Rogers/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello, my name is Dace");
+
+        System.out.println("Hello, Dace my name is Mandy");
     }
 }
